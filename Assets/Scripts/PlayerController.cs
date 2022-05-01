f3eae5a46a8729c48babe0da69cfd568d32c5314
Emmanuella Ogbuagu <emmanuella240698@gmail.com>
2022-05-01 22:02:44 +0100

    Added Player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float horizontalInput;
+    [SerializeField]
+    private float speed=10f;
+    [SerializeField]
+    private float xBounds=12f;
     void Start()
     {
         
@@ -13,6 +18,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if ( transform.position.x < -xBounds)
+        {
+            transform.position =new Vector3(-xBounds,transform.position.y,transform.position.z);
+        }
+        if (transform.position.x > xBounds)
+        {
+            transform.position =new Vector3(xBounds,transform.position.y,transform.position.z);
+        }
+        horizontalInput= Input.GetAxis("Horizontal");
+        transform.Translate(horizontalInput*Time.deltaTime*speed* Vector3.right);
+        
         
     }
 }
